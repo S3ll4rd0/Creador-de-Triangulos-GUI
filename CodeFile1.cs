d1db15444da1f5f21e26f2a50748c61abfce8fe6
Antonio Nicolás Salmerón Rubio <54323367+S3ll4rd0@users.noreply.github.com>
2022-07-03 22:23:27 +0200

    Add files to repository via upload
    Añado los ficheros del proyecto al repositorio

---
+++ CodeFile1.cs
@@ -0,0 +1,70 @@
+﻿public class Triangulo
+{
+    private double baseTri;
+    private double alturaTri;
+    private double ladoTri;
+
+    private string ladoMayor;
+    public Triangulo() { }
+
+    public Triangulo(double alturaTri, double baseTri, double ladoTri)
+    {
+        this.baseTri = baseTri;
+        this.alturaTri = alturaTri;
+        this.ladoTri = ladoTri;
+    }
+
+    public double Area(double alturaTri, double baseTri)
+    {
+        return ((baseTri * alturaTri)/2);
+    }
+
+    public bool EsEquilatero(double alturaTriangulo, double baseTriangulo, double ladoTriangulo)
+    {
+        if (ladoTriangulo == alturaTriangulo)
+        {
+            if (ladoTriangulo == baseTriangulo)
+            {
+                //Console.WriteLine("El triángulo es equilatero");
+                return true;
+            }
+            return false;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    public string LadoMayor(double alturaTriangulo, double baseTriangulo, double ladoTriangulo)
+    {
+        if (ladoTriangulo > baseTriangulo)
+        {
+            if (ladoTriangulo > alturaTriangulo)
+            {
+                ladoMayor = "El lado mayor es el Lado del triángulo: " + ladoTriangulo;
+                return ladoMayor;
+            }
+            else
+            {
+                ladoMayor = "El lado mayor es la altura del triángulo: " + alturaTriangulo;
+                return ladoMayor;
+            }
+        }
+        else
+        {
+            if (baseTriangulo > alturaTriangulo)
+            {
+                ladoMayor = "El lado mayor es la base del triángulo: " + baseTriangulo;
+                return ladoMayor;
+            }
+            else
+            {
+                ladoMayor = "El lado mayor es la altura del triángulo: " + alturaTriangulo;
+                return ladoMayor;
+            }
+        }
+    }
+
+
+}
---
+++ Form1.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Triangulo18B
+{
+    public partial class Form1 : Form
+    {
+        Triangulo myTriangle;
+
+        public Form1()
+        {
+            InitializeComponent();
+            myTriangle = new Triangulo();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            myTriangle = new Triangulo(
+                Convert.ToDouble(textBox1.Text), 
+                Convert.ToDouble(textBox2.Text),
+                Convert.ToDouble(textBox3.Text));
+            button2.Enabled = true;
+            button3.Enabled = true;
+            button4.Enabled = true;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if  (myTriangle.EsEquilatero(
+                Convert.ToDouble(textBox1.Text),
+                Convert.ToDouble(textBox2.Text),
+                Convert.ToDouble(textBox3.Text)))
+            {
+                MessageBox.Show("Si es equilatero");
+            }
+            else
+            {
+                MessageBox.Show("No es equilatero");
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(
+            myTriangle.LadoMayor(
+                Convert.ToDouble(textBox1.Text),
+                Convert.ToDouble(textBox2.Text),
+                Convert.ToDouble(textBox3.Text)));
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("El area del triángulo es: " +
+            myTriangle.Area(
+                Convert.ToDouble(textBox1.Text),
+                Convert.ToDouble(textBox2.Text)));
+        }
+    }
+}
